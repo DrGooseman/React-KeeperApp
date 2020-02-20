1b9bb3b1c292dc15f6319afbc266a48dc2ee37af
DrGooseman <jamesjquinn12@gmail.com>
2020-02-20 12:42:54 +0300

    Notes are now components and are created from an array

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header.jsx";
 import Note from "./Note.jsx";
 import Footer from "./Footer.jsx";
+import notes from "./../notes";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Note></Note>
+      {notes.map(note => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer></Footer>
     </div>
   );
---
+++ src/notes.js
@@ -0,0 +1,33 @@
+const notes = [
+  {
+    key: 1,
+    title: "Note 1",
+    content: "This is note 1, note 1 is this."
+  },
+  {
+    key: 2,
+    title: "Note 2",
+    content: "This is note 2, note 2 is this."
+  },
+  {
+    key: 3,
+    title: "Note 3",
+    content: "This is note 3, note 3 is this."
+  },
+  {
+    key: 4,
+    title: "Note 4",
+    content: "This is note 4, note 4 is this."
+  },
+  {
+    key: 5,
+    title: "Note 5",
+    content: "This is note 5, note 5 is this."
+  },
+  {
+    key: 6,
+    title: "Note 6",
+    content: "This is note 6, note 6 is this."
+  }
+];
+export default notes;
