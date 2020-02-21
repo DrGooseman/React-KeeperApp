d05ed6a6bc6ccb17f5dac563f523736eb14c117c
DrGooseman <jamesjquinn12@gmail.com>
2020-02-21 17:12:40 +0300

    Adds ability to create and delete notes

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote({ ...note, [name]: value });
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          onChange={handleChange}
+          value={note.content}
+        />
+        <button
+          onClick={event => {
+            props.handleCreateArea(note);
+            setNote({ title: "", content: "" });
+            event.preventDefault();
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header.jsx";
 import Note from "./Note.jsx";
 import Footer from "./Footer.jsx";
-import notes from "./../notes";
+import startingNotes from "./../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState(startingNotes);
+
+  function addNote(note) {
+    const newNote = { key: notes.length + 1, ...note };
+    setNotes([...notes, newNote]);
+  }
+
+  function deleteNote(index) {
+    const newNotes = notes.filter(note => note.key !== index);
+    setNotes(newNotes);
+  }
+
   return (
     <div>
       <Header></Header>
+      <CreateArea handleCreateArea={addNote} />
       {notes.map(note => (
-        <Note key={note.key} title={note.title} content={note.content} />
+        <Note
+          key={note.key}
+          index={note.key}
+          title={note.title}
+          content={note.content}
+          handleClick={deleteNote}
+        />
       ))}
       <Footer></Footer>
     </div>
